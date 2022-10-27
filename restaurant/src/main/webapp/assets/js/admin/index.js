17252a279cb6bb1afc2dcec15c85b01c130ffc58
KimBeomKi <KimBeomKi@DESKTOP-K265MDK>
2022-10-28 00:12:22 +0900

    ※ JSP 파일에 있던 스크립트 js파일로 이동

--- restaurant/src/main/webapp/assets/js/admin/index.js
+++ restaurant/src/main/webapp/assets/js/admin/index.js
@@ -24,4 +24,44 @@ function userDelete(user_seq) {
 	});
 
 	
-
+// 선택한 회원 탈퇴
+function selectUserDelete() {
+		var valueArr = new Array();
+		var list = $("input[name='rowCheck']");
+		
+		for (var i = 0; i < list.length; i++) {
+			// 선택된 체크박스가 있으면 배열에 값 저장
+			if (list[i].checked) {
+				valueArr.push(list[i].value);
+			}
+		}
+		if (valueArr.length == 0) {
+			alert("탈퇴시킬 회원을 선택해주세요.");
+			return;
+		} else {
+			if (confirm("선택한 회원을 탈퇴시키겠습니까?")) {
+				$.ajax ({
+					type	:	"POST",
+					url		:	"/admin/selectUserDelete.do",
+					traditional : true,		// 배열로 보낼때 사용
+					data	:	{
+						valueArr: valueArr
+					},
+					success : function(data) {
+						console.log("Data	::	" + data);
+						if (data = 1) {
+							alert("회원탈퇴가 완료되었습니다.");
+							location.reload();
+						} else {
+							alert("회원탈퇴를 실패하였습니다.");
+							return;
+						}
+					},
+					error : function(error, xhr) {
+						alert("error!!\n" + error + "	" + xhr);
+						return;
+					}
+				})	
+			}
+		}
+	}
